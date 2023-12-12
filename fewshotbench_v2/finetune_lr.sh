81b45882a18d03050c98bd670b7d14338a1e5aab
Hanrong Hu <hanronghu98@gmail.com>
2023-12-12 23:01:01 +0000

    more lr

--- fewshotbench_v2/finetune_lr.sh
+++ fewshotbench_v2/finetune_lr.sh
@@ -1,7 +1,8 @@
 method=maml
 
 if [ "$method" == "maml" ]; then
-    lrSamples=(0.02 0.05 0.1)
+    #lrSamples=(0.02 0.05 0.1)
+    lrSamples=(0.07 0.2)
     for lr in "${lrSamples[@]}";do
         python run.py experiment_subset.feature_class=Promoter exp.name="atacseq_adult_promoter_${method}_${lr}lr" method.maml_inner_lr=${lr} 
     done
