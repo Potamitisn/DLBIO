d570085c5e836e0367238dfe7ff48b653c4f0521
Hanrong Hu <hanronghu98@gmail.com>
2023-12-12 22:49:04 +0000

    fix a typo

--- fewshotbench_v2/finetune_dim.sh
+++ fewshotbench_v2/finetune_dim.sh
@@ -3,5 +3,5 @@ dims=(32, 64, 128)
 for dim in "${dims[@]}";do
     python run.py experiment_subset.feature_class=Promoter \
     exp.name="atacseq_adult_promoter_${method}_dim_${dim}_${dim}" \
-    backbone.layer_dim=[${dim}, ${dim}] 
+    backbone.layer_dim=[${dim},${dim}] 
 done
