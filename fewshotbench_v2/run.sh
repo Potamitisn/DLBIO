51f4cbd8193866116bc0280f4c16e884c17ec449
Potamitisn <nearchosp@outlook.com>
2023-12-12 22:30:08 +0000

    ablation update

--- fewshotbench_v2/run.sh
+++ fewshotbench_v2/run.sh
@@ -1,14 +1,17 @@
-method=maml
+method=baseline
 param=n_shot # [n_way, n_shot]
-nWaySamples=(1 10 20 35 50)
-nShotSamples=(1 10 20 50 100)
+nWaySamples=(1 35 50)
+nShotSamples=(50 100)
+
 
 if [ "$param" == "n_way" ]; then
+    echo "Studying ${method} for different n_way"
     for nWay in "${nWaySamples[@]}"
     do
         python run.py experiment_subset.feature_class=Promoter exp.name="atacseq_adult_promoter_${method}_${nWay}way" method=${method} n_way=${nWay} 
     done
 else 
+    echo "Studying ${method} for different n_shot"
     for nShot in "${nShotSamples[@]}"
     do
         python run.py experiment_subset.feature_class=Promoter exp.name="atacseq_adult_promoter_${method}_${nShot}shot" method=${method} n_shot=${nShot} 
