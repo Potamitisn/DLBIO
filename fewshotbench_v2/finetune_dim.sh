2932714d2d5791823ec255200523f75632760b6f
Hanrong Hu <hanronghu98@gmail.com>
2023-12-13 00:15:22 +0000

    typo

--- fewshotbench_v2/finetune_dim.sh
+++ fewshotbench_v2/finetune_dim.sh
@@ -1,5 +1,5 @@
 method=maml
-dims=(32, 64, 128)
+dims=(32 128 256)
 for dim in "${dims[@]}";do
     python run.py experiment_subset.feature_class=Promoter \
     exp.name="atacseq_adult_promoter_${method}_dim_${dim}_${dim}" \
